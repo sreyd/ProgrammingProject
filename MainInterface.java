bc92857e2a99ccc5e55f0905e35b0470300bf195
aryeeteyelaine <aryeeteyelaine@yahoo.com>
2016-03-27 22:54:08 -0500

    Basic window

---
+++ MainInterface.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class MainInterface {
+
+	public static void main (String[]args) {
+		//Create and set up the window.
+	    JFrame frame = new JFrame("Encrypt App");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    
+	  //Create the menu bar.  Make it have a green background.
+        JMenuBar greenMenuBar = new JMenuBar();
+        greenMenuBar.setOpaque(true);
+        greenMenuBar.setBackground(new Color(154, 165, 127));
+        greenMenuBar.setPreferredSize(new Dimension(200, 20));
+
+        //Create a yellow label to put in the content pane.
+        JLabel yellowLabel = new JLabel();
+        yellowLabel.setOpaque(true);
+        yellowLabel.setBackground(new Color(248, 213, 131));
+        yellowLabel.setPreferredSize(new Dimension(200, 180));
+
+        //Set the menu bar and add the label to the content pane.
+        frame.setJMenuBar(greenMenuBar);
+        frame.getContentPane().add(yellowLabel, BorderLayout.CENTER);
+
+	  //Display the window.
+	    frame.pack();
+	    frame.setVisible(true);
+	}
+}
